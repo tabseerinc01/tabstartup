974679989bd579928db756c53e3e5e15d1e77378
Shah Mubaruk <shahmubaruk0@gmail.com>
2026-01-16 14:56:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3911951123-2080a",
+  "appId": "1:750906811745:web:7309824ab8b79c9b4b1120",
+  "apiKey": "AIzaSyAWzYLFpsw7qaBA5aSudqdK7EF-MnpUG-c",
+  "authDomain": "studio-3911951123-2080a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "750906811745"
+};
