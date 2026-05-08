18d91a0a07cb27f212740d9886938f5fad1c6305
Shah Mubaruk <shahmubaruk0@gmail.com>
2026-05-08 18:00:42 +0000

    Try fixing this error: `Build Error: Export useCollection doesn't exist

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -32,6 +32,8 @@ export function getSdks(firebaseApp: FirebaseApp) {
 
 export * from './provider';
 export * from './client-provider';
+export * from './firestore/use-collection';
+export * from './firestore/use-doc';
 export * from './non-blocking-updates';
 export * from './non-blocking-login';
 export * from './errors';
